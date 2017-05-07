cbc85dc837c2a262e938edc517e46947f7cd9609
nozomin64 <nozomin.bear.6.4@gmail.com>
2017-05-07 02:32:37 +0000

    add column

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -6,12 +6,14 @@
 
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status') !!}
+
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
 
         {!! Form::submit('更新') !!}
 
     {!! Form::close() !!}
-
 
 @endsection
